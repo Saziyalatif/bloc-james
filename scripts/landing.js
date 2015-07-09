9023e5d59f4ec9964249fa9c9d925afe6f8e7036
Saziya Latif <saziyalatif@gmail.com>
2015-07-09 15:43:18 -0700

    animations

---
+++ scripts/landing.js
@@ -0,0 +1,15 @@
+/*
+
+for (i = 0; i < cars.length; i++) { 
+    text += cars[i] + "<br>";
+}
+
+*/
+var animatePoints = function() {
+    var revealPoints = function(index) {
+
+ 
+                for (var i = 0; i < points.length; i++) {
+        revealPoints(i);
+                    
+             };
